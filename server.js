408dc2be1a5896e07618304d8ed906dc8f8ee77e
girijaQuloi <girija@quloi.com>
2026-05-29 21:14:30 +0530

    swagger api fix for vercel production

--- server.js
+++ server.js
@@ -16,14 +16,23 @@ const port = 5000;
 app.use(express.json())
 
 // Swagger API 
-app.use("/api-docs", express.static(path.join(__dirname, "../node_modules/swagger-ui-dist")));
-
 app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec)
+  "/api-docs-assets",
+  express.static(path.join(__dirname, "../node_modules/swagger-ui-dist"))
 );
 
+// Swagger UI
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    customCssUrl: "/api-docs-assets/swagger-ui.css",
+    customJs: [
+      "/api-docs-assets/swagger-ui-bundle.js",
+      "/api-docs-assets/swagger-ui-standalone-preset.js",
+    ],
+  })
+);
 
 // Pilot server test
 app.get("/", (req, res) => {
